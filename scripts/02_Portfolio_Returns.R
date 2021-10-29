0e20587c09c4ab788d36305ed4b3c32c11944b1d
Juan Malaver <led.juan@hotmail.com>
2021-10-28 21:37:02 -0500

    adding 5 stocks optimizing visualization

--- scripts/02_Portfolio_Returns.R
+++ scripts/02_Portfolio_Returns.R
@@ -67,3 +67,47 @@ hchart(hc_portfolio,
   hc_title(text = "Portfolio Returns Distribution") %>%
   hc_add_theme(hc_theme_flat()) %>%
   hc_exporting(enabled = TRUE)
+
+# 10 STOCK PORTFOLIO ---
+
+## storing tickers in vector
+symbols <- c("AAPL", "MSFT", "", "XLK", "XLV")
+
+## retrieving stock prices
+prices <- symbols %>%
+  tq_get(get = "stock.prices",
+         from = "2016-01-01")
+
+## generating monthly returns
+asset_returns <- prices %>%
+  group_by(symbol) %>%
+  tq_transmute(adjusted,
+               periodReturn,
+               period = "monthly",
+               type = "log",
+               col_rename = "monthly.returns")
+
+## storing weights in vector
+wts <- c(0.4, 0.15, 0.15, 0.15, 0.15)
+
+## generating portfolio returns
+portfolio_returns <- asset_returns %>%
+  tq_portfolio(assets_col = symbol,
+               returns_col = monthly.returns,
+               weights = wts,
+               col_rename = "returns",
+               rebalance_on = "months") %>%
+  tk_xts()
+
+## visualizing portfolio returns
+#+ Monthly Portfolio Returns
+highchart(type = "stock") %>%
+  hc_title(text = "Portfolio Monthly Returns") %>%
+  hc_add_series(portfolio_returns$returns,
+                name = "Rebalanced Monthly",
+                color = "cornflowerblue") %>%
+  hc_add_theme(hc_theme_flat()) %>%
+  hc_navigator(enabled = FALSE) %>%
+  hc_scrollbar(enabled = FALSE) %>%
+  hc_exporting(enabled = TRUE) %>%
+  hc_legend(enabled = TRUE)
